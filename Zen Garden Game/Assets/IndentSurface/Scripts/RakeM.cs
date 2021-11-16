008ec30d79d383eb571a6bbccaec928e26154877
d1story <55068653+d1story@users.noreply.github.com>
2021-11-16 08:06:53 -0700

    Adding RayCast detect objects

--- Zen Garden Game/Assets/IndentSurface/Scripts/RakeM.cs
+++ Zen Garden Game/Assets/IndentSurface/Scripts/RakeM.cs
@@ -7,28 +7,21 @@ namespace Wacki.IndentSurface
     public class RakeM : MonoBehaviour
     {
         private bool _mouseDrag = false;
-        public float movementSpeed, rotationSpeed, consty, stopT;
+        public float movementSpeed, rotationSpeed, consty, stopT, X, Y, Z;
         public AudioSource SandNoise;
         public Collider[] teeth;
-        private float T;
-        bool bump(Collider rake)
+        void bump(Collider rake)
         {
-            Debug.Log("In");
             Collider[] into = Physics.OverlapBox(rake.bounds.center, rake.bounds.extents, rake.transform.rotation);
 
             foreach (Collider other in into)
             {
-                //if (other.gameObject.GetComponent<ProdN>() != null)
-                //{
+                if (other.gameObject.GetComponent<ProdNoise>() != null)
+                {
                     //Debug.Log("InIn");
-                    //other.gameObject.GetComponent<ProdN>().PPlay();
-                //}
+                    //other.gameObject.GetComponent<ProdNoise>().PPlay();
+                }
             }
-            if (into.Length > 6)
-            {
-                return true;
-            }
-            return false;
         }
 
         private void contact(Collider col)
@@ -39,54 +32,52 @@ namespace Wacki.IndentSurface
                 var Obj = hitRake.collider.gameObject;
                 if (Obj.GetComponent<IndentDraw>() != null)
                 {
-                    Debug.Log("alksdjfalk");
                     Obj.GetComponent<IndentDraw>().IndentAt(hitRake); //make indent in sand
                 }
             }
         }
 
-        void Start()
-        {
-            T = Time.time - stopT;
-        }
-
         void Update()
         {
-            if (Time.time > T + stopT)
+            var rake = GetComponent<Rigidbody>();
+            //raycast of mouse
+            RaycastHit hit;
+
+            //checking for mouse action
+            _mouseDrag = Input.GetMouseButton(0);
+
+            //making indent
+            foreach (Collider tooth in teeth)
+                contact(tooth);
+
+            //making sand noises
+            //if (tranform.velocity.magnitude > 1 && !SandNoise.isPlaying)
+            //SandNoise.Play();
+            if (_mouseDrag && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
             {
-                Debug.Log("Start");
-                var rake = GetComponent<Rigidbody>();
-                //raycast of mouse
-                RaycastHit hit;
+                Vector3 To = hit.point;
+                To.y = consty;
+                Vector3 relativePos = transform.position - To;
+                Quaternion rotation = Quaternion.LookRotation(relativePos);
 
-                //checking for mouse action
-                _mouseDrag = Input.GetMouseButton(0);
-
-                //making indent
-                foreach (Collider tooth in teeth)
-                    contact(tooth);
-
-                //making sand noises
-                //if (tranform.velocity.magnitude > 1 && !SandNoise.isPlaying)
-                //SandNoise.Play();
-                if (_mouseDrag && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+                //find start cus unity big gay
+                Vector3 from = new Vector3(transform.position.x - X, transform.position.y - Y, transform.position.z - Z);
+                Debug.DrawRay(from, To-from, Color.white);
+                //check if there is some thing in the way
+                RaycastHit temp;
+                if (Physics.Raycast(from, To-from, out temp))
                 {
-                    Debug.Log("Start");
-                    Vector3 To = hit.point;
-                    To.y = consty;
-                    Vector3 relativePos = transform.position - new Vector3(To.x, consty, To.z);
-                    Quaternion rotation = Quaternion.LookRotation(relativePos);
-
-
-                    Quaternion currentQ = transform.localRotation;
-                    Vector3 currentV = transform.localPosition;
-                    transform.rotation = Quaternion.Lerp(currentQ, rotation, rotationSpeed * Time.deltaTime);
-                    transform.position = Vector3.Lerp(currentV, To, movementSpeed * Time.deltaTime);
+                    To = temp.point;
+                    Debug.DrawRay(from, To-from, Color.red);
+                    Debug.Log(temp.collider.gameObject.name);
                 }
-                if (bump(rake.GetComponent<Collider>()))
-                {
-                    T = Time.time;
-                }
+                To.y = consty;
+
+                Quaternion currentQ = transform.localRotation;
+                Vector3 currentV = transform.localPosition;
+                transform.rotation = Quaternion.Lerp(currentQ, rotation, rotationSpeed * Time.deltaTime);
+                transform.position = Vector3.Lerp(currentV, To, movementSpeed * Time.deltaTime);
+                bump(rake.GetComponent<Collider>());
             }
         }
     }
