ae4a32b0f4c23726959d8a9316bc70db694304af
Allan <40648867+GChroma@users.noreply.github.com>
2021-11-19 20:30:02 -0700

    did some stuff idk

--- Zen Garden Game/Assets/Lighting/LightingManager.cs
+++ Zen Garden Game/Assets/Lighting/LightingManager.cs
@@ -54,7 +54,7 @@ public class LightingManager : MonoBehaviour
         {
             DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
 
-            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 90f, 45f));
         }
 
     }
